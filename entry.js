af686e18c6d6c48be8d45c90a10b7b9c4f073168
i3 <453072341@>
2016-11-21 17:39:58 +0800

    修改了背景图片 调整了苹果图标下移

--- entry.js
+++ entry.js
@@ -2,7 +2,7 @@ require('./css/iconfont.css'); // 载入 style.css
 require('./css/main.css'); // 载入 style.css
 
 ////require('./ie/respond.min.js');
-require('./jquery.js');
+//require('./jquery.js');
 require('./jquery.leanModal.min.js');
 require('./ie/html5shiv.js');
 //require('./js/jquery');
